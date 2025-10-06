73980121b8947dee60f876f7b103f756e7b4408b
Manuel Schneider <manuelschneid3r@gmail.com>
2025-10-06 13:16:24 +0200

    feat: Use SDK manually installed into venv

--- __init__.py
+++ __init__.py
@@ -1,119 +1,184 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2024 Manuel Schneider
-"""
-This plugin is based on [virtualbox-python](https://pypi.org/project/virtualbox/) and needs the 'vboxapi' module which
-is part of the VirtualBox SDK. Some distributions package the SDK, e.g. Arch has
-[virtualbox-sdk](https://archlinux.org/packages/extra/x86_64/virtualbox-sdk/).
-"""
-
-import virtualbox
-from virtualbox.library import LockType, MachineState
 
 from albert import *
+from sys import platform
+from contextlib import contextmanager
 
-md_iid = "3.0"
+md_iid = "4.0"
 md_version = "2.1"
 md_name = "VirtualBox"
 md_description = "Manage your VirtualBox machines"
 md_license = "MIT"
 md_url = "https://github.com/albertlauncher/albert-plugin-python-virtualbox"
+md_readme_url = "https://github.com/albertlauncher/albert-plugin-python-virtualbox/blob/main/README.md"
 md_authors = ["@ManuelSchneid3r"]
-md_lib_dependencies = ['virtualbox']
 
 
-def startVm(vm):
-    try:
-        with virtualbox.Session() as session:
-            progress = vm.launch_vm_process(session, 'gui', [])
-            progress.wait_for_completion()
-    except Exception as e:
-        warning(str(e))
-
-
-def acpiPowerVm(vm):
-    with vm.create_session(LockType.shared) as session:
-        session.console.power_button()
-
-
-def stopVm(vm):
-    with vm.create_session(LockType.shared) as session:
-        session.console.power_down()
-
-
-def saveVm(vm):
-    with vm.create_session(LockType.shared) as session:
-        session.machine.save_state()
-
-
-def discardSavedVm(vm):
-    with vm.create_session(LockType.shared) as session:
-        session.machine.discard_save_state(True)
-
-
-def resumeVm(vm):
-    with vm.create_session(LockType.shared) as session:
-        session.console.resume()
-
-
-def pauseVm(vm):
-    with vm.create_session(LockType.shared) as session:
-        session.console.pause()
-
-
-class Plugin(PluginInstance, TriggerQueryHandler):
+class Plugin(PluginInstance, GlobalQueryHandler):
 
     def __init__(self):
         PluginInstance.__init__(self)
-        TriggerQueryHandler.__init__(self)
-        self.iconUrls = ["xdg:virtualbox", ":unknown"]
+        GlobalQueryHandler.__init__(self)
+
+        if platform == "darwin":
+            self.iconUrls = ["qfip:/Applications/VirtualBox.app"]
+        elif platform == "linux":
+            self.iconUrls = ["xdg:virtualbox"]
+        else:
+            raise NotImplementedError("Unhandled platform. Port me!")
+
+        try:
+            import vboxapi
+        except ImportError:
+            self.installSDK()
+            try:
+                import vboxapi
+            except ImportError:
+                raise ImportError(f"Failed to import vboxapi despite successful SDK installation.")
+
+
+        self.vbox_manager = vboxapi.VirtualBoxManager(None, None)
+        self.vbox_constants = self.vbox_manager.constants
+        self.vbox = self.vbox_manager.getVirtualBox()
+
+    def installSDK(self):
+        import hashlib
+        import os
+        import shutil
+        import zipfile
+        import urllib.request
+        import subprocess
+
+        cache_location = self.cacheLocation()
+        cache_location.mkdir(parents=True)
+
+        try:
+            url = "https://download.virtualbox.org/virtualbox/7.0.26/VirtualBoxSDK-7.0.26-168464.zip"
+            sha_expected = "504a5a7ea468ad1d19041379c8204287c73eb18926b51d51f269269dbcde9b96"
+            filename = cache_location / os.path.basename(url)
+
+            info(f"Downloading SDK ...")
+            urllib.request.urlretrieve(url, filename)
+
+            sha256 = hashlib.sha256()
+            with open(filename, "rb") as f:
+                for block in iter(lambda: f.read(8192), b""):
+                    sha256.update(block)
+            sha_actual = sha256.hexdigest()
+
+            if sha_actual.lower() != sha_expected.lower():
+                raise ValueError("Downloaded file failed integrity check.")
+
+            info(f"Extracting SDK ...")
+            with zipfile.ZipFile(filename, 'r') as zip_ref:
+                zip_ref.extractall(cache_location)
+
+            info(f"Installing SDK ...")
+            if platform == "darwin":
+                VBOX_INSTALL_PATH = "/Applications/VirtualBox.app/Contents/MacOS/"
+            elif platform == "linux":
+                VBOX_INSTALL_PATH = "/usr/lib/virtualbox"
+            else:
+                raise NotImplementedError("Unhandled platform. See README.")
+
+            # 7.2 has pyproject toml echo 'VBOX_INSTALL_PATH="{VBOX_INSTALL_PATH}" pip install "{cache_location / "sdk/installer/vboxapi"}"';
+            # Okt 25 TODO
+            script = f"""
+                source "{self.dataLocation().parent}/python/venv/bin/activate"
+                pip install setuptools
+                cd "{cache_location / "sdk/installer"}"
+                VBOX_INSTALL_PATH="{VBOX_INSTALL_PATH}" python vboxapisetup.py install
+            """
+            subprocess.run(["sh", "-c", script], check=True)
+
+        finally:
+            shutil.rmtree(cache_location)
 
     def defaultTrigger(self):
         return 'vbox '
 
-    def synopsis(self, query):
-        return "<machine name>"
-
     def configWidget(self):
-        return [
-            {
-                'type': 'label',
-                'text': __doc__.strip(),
-                'widget_properties': {
-                    'textFormat': 'Qt::MarkdownText'
-                }
-            }
-        ]
+        return [{
+            'type': 'label',
+            'text': f"Please read the [README.md]({md_readme_url}) for details.",
+            'widget_properties': { 'textFormat': 'Qt::MarkdownText' }
+        }]
 
-    def handleTriggerQuery(self, query):
+    def buildItem(self, vm):
+        actions = []
+        state = vm.state
+        if state in [self.vbox_constants.MachineState_PoweredOff, self.vbox_constants.MachineState_Aborted]:
+            actions.append(Action("startvm", "Start virtual machine", lambda m=vm: self.startVm(m)))
+        if state == self.vbox_constants.MachineState_Saved:
+            actions.append(Action("restorevm", "Start saved virtual machine", lambda m=vm: self.startVm(m)))
+            actions.append(Action("discardvm", "Discard saved state", lambda m=vm: self.discardSavedVm(m)))
+        if state == self.vbox_constants.MachineState_Running:
+            actions.extend([
+                Action("savevm", "Save virtual machine", lambda m=vm: self.saveVm(m)),
+                Action("poweroffvm", "Power off via ACPI", lambda m=vm: self.acpiPowerVm(m)),
+                Action("stopvm", "Turn off virtual machine", lambda m=vm: self.stopVm(m)),
+                Action("pausevm", "Pause virtual machine", lambda m=vm: self.pauseVm(m))
+            ])
+        if state == self.vbox_constants.MachineState_Paused:
+            actions.append(Action("resumevm", "Resume virtual machine", lambda m=vm: self.resumeVm(m)))
+
+        return StandardItem(
+            id=vm.id,
+            text=vm.name,
+            subtext=self.vbox_manager.getEnumValueName("MachineState", vm.state),
+            inputActionText=vm.name,
+            iconUrls=self.iconUrls,
+            actions=actions
+        )
+
+    def handleGlobalQuery(self, query):
         items = []
-        pattern = query.string.strip().lower()
-        try:
-            for vm in filter(lambda vm: pattern in vm.name.lower(), virtualbox.VirtualBox().machines):
-                actions = []
-                if vm.state == MachineState.powered_off or vm.state == MachineState.aborted:  # 1 # 4
-                    actions.append(Action("startvm", "Start virtual machine", lambda m=vm: startVm(m)))
-                if vm.state == MachineState.saved:  # 2
-                    actions.append(Action("restorevm", "Start saved virtual machine", lambda m=vm: startVm(m)))
-                    actions.append(Action("discardvm", "Discard saved state", lambda m=vm: discardSavedVm(m)))
-                if vm.state == MachineState.running:  # 5
-                    actions.append(Action("savevm", "Save virtual machine", lambda m=vm: saveVm(m)))
-                    actions.append(Action("poweroffvm", "Power off via ACPI event (Power button)", lambda m=vm: acpiPowerVm(m)))
-                    actions.append(Action("stopvm", "Turn off virtual machine", lambda m=vm: stopVm(m)))
-                    actions.append(Action("pausevm", "Pause virtual machine", lambda m=vm: pauseVm(m)))
-                if vm.state == MachineState.paused:  # 6
-                    actions.append(Action("resumevm", "Resume virtual machine", lambda m=vm: resumeVm(m)))
+        matcher = Matcher(query.string)
+        for vm in self.vbox_manager.getArray(self.vbox, "machines"):
+            if m := matcher.match(vm.name):
+                items.append(RankItem(self.buildItem(vm), m))
+        return items
 
-                items.append(
-                    StandardItem(
-                        id=vm.__uuid__,
-                        text=vm.name,
-                        subtext="{vm.state}".format(vm=vm),
-                        inputActionText=vm.name,
-                        iconUrls=self.iconUrls,
-                        actions=actions
-                    )
-                )
+    def startVm(self, vm):
+        try:
+            info(f"Starting VM {vm.name} {type(vm)}...")
+            session = self.vbox_manager.getSessionObject(self.vbox)
+            progress = vm.launchVMProcess(session, 'gui', '')
+            progress.waitForCompletion(-1)
         except Exception as e:
             warning(str(e))
 
-        query.add(items)
+    @contextmanager
+    def sharedLockedSession(self, vm):
+        session = self.vbox_manager.getSessionObject(self.vbox)
+        try:
+            vm.lockMachine(session, self.vbox_constants.LockType_Shared)
+            yield session
+        finally:
+            session.unlockMachine()
+
+    def acpiPowerVm(self, vm):
+        with self.sharedLockedSession(vm) as s:
+            s.console.powerButton()
+
+    def stopVm(self, vm):
+        with self.sharedLockedSession(vm) as s:
+            s.console.powerDown()
+
+    def saveVm(self, vm):
+        with self.sharedLockedSession(vm) as s:
+            s.console.saveState()
+
+    def discardSavedVm(self, vm):
+        with self.sharedLockedSession(vm) as s:
+            s.machine.discardSavedState()
+
+    def resumeVm(self, vm):
+        with self.sharedLockedSession(vm) as s:
+            s.console.resume()
+
+    def pauseVm(self, vm):
+        with self.sharedLockedSession(vm) as s:
+            s.console.pause()
+
